00a797f8e0235988b2341ab6280d109b41fd60d4
Muna Yusuf <muna369241@hotmail.com>
2026-03-12 08:44:35 +0000

    add git ignore

--- reporting_backend/reports/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import ReportedAccount, ContentItem, Report
-# Register your models here.
-
-admin.site.register(ReportedAccount)
-admin.site.register(ContentItem)
-admin.site.register(Report)
-
